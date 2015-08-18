f881bb502f2f2c46079d5f15fa2a78cd96338c83
Steven Logue <bitvector2@gmail.com>
2015-08-18 14:06:21 -0700

    added Undertow hello world to Main

--- src/main/java/org/bitvector/microservice2/Main.java
+++ src/main/java/org/bitvector/microservice2/Main.java
@@ -1,6 +1,10 @@
 package org.bitvector.microservice2;
 
 import akka.actor.*;
+import io.undertow.Undertow;
+import io.undertow.server.HttpHandler;
+import io.undertow.server.HttpServerExchange;
+import io.undertow.util.Headers;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import scala.concurrent.duration.Duration;
@@ -31,6 +35,19 @@ public class Main {
             logger.error("Could not load properties file: " + propertiesFile, e);
             System.exit(1);
         }
+
+
+        Undertow server = Undertow.builder()
+                .addHttpListener(8080, "localhost")
+                .setHandler(new HttpHandler() {
+                    @Override
+                    public void handleRequest(final HttpServerExchange exchange) throws Exception {
+                        exchange.getResponseHeaders().put(Headers.CONTENT_TYPE, "text/plain");
+                        exchange.getResponseSender().send("Hello World");
+                    }
+                }).build();
+        server.start();
+
 
         // Create the 'helloakka' actor system
         final ActorSystem system = ActorSystem.create("helloakka");
