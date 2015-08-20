d8966599aad8f7db45d7ac4068f2d4b9fd72d650
Steven Logue <bitvector2@gmail.com>
2015-08-19 19:00:32 -0700

    :whitespace:

--- src/main/java/org/bitvector/microservice2/HttpActor.java
+++ src/main/java/org/bitvector/microservice2/HttpActor.java
@@ -1,63 +1,1 @@
-package org.bitvector.microservice2;
-
-import akka.actor.UntypedActor;
-import akka.event.Logging;
-import akka.event.LoggingAdapter;
-import io.undertow.Handlers;
-import io.undertow.Undertow;
-import io.undertow.server.HttpHandler;
-import io.undertow.server.HttpServerExchange;
-import io.undertow.server.RoutingHandler;
-import io.undertow.util.Headers;
-import io.undertow.util.Methods;
-
-import java.io.Serializable;
-
-public class HttpActor extends UntypedActor {
-    private LoggingAdapter log = Logging.getLogger(getContext().system(), this);
-    private Undertow server = null;
-
-    private void start() {
-        RoutingHandler rootHandler = Handlers.routing()
-                .add(Methods.HEAD, "/foo", exchange -> {
-                    exchange.getResponseSender().close();
-                })
-                .add(Methods.GET, "/foo", exchange -> {
-                    exchange.getResponseHeaders().put(Headers.CONTENT_TYPE, "text/plain");
-                    exchange.getResponseSender().send("foo\n");
-                })
-                .add(Methods.GET, "/foo/{id}", exchange -> {
-                    exchange.getResponseHeaders().put(Headers.CONTENT_TYPE, "text/plain");
-                    exchange.getResponseSender().send("foo" + exchange.getQueryParameters().get("id") + "\n");
-                });
-
-        server = Undertow.builder()
-                .addHttpListener(8080, "0.0.0.0", rootHandler)
-                .build();
-
-        server.start();
-        log.info("HttpActor Started");
-    }
-
-    private void stop() {
-        server.stop();
-        log.info("HttpActor Stopped");
-    }
-
-    public void onReceive(Object message) throws Exception {
-        if (message instanceof Start) {
-            this.start();
-        } else if (message instanceof Stop) {
-            this.stop();
-        } else {
-            unhandled(message);
-        }
-    }
-
-    public static class Start implements Serializable {
-    }
-
-    public static class Stop implements Serializable {
-    }
-
-}
+package org.bitvector.microservice2;import akka.actor.UntypedActor;import akka.event.Logging;import akka.event.LoggingAdapter;import io.undertow.Handlers;import io.undertow.Undertow;import io.undertow.server.RoutingHandler;import io.undertow.util.Headers;import io.undertow.util.Methods;import java.io.Serializable;public class HttpActor extends UntypedActor {    private LoggingAdapter log = Logging.getLogger(getContext().system(), this);    private Undertow server = null;    private void start() {        RoutingHandler rootHandler = Handlers.routing()                .add(Methods.HEAD, "/foo", exchange -> {                    exchange.getResponseSender().close();                })                .add(Methods.GET, "/foo", exchange -> {                    exchange.getResponseHeaders().put(Headers.CONTENT_TYPE, "text/plain");                    exchange.getResponseSender().send("foo\n");                })                .add(Methods.GET, "/foo/{id}", exchange -> {                    exchange.getResponseHeaders().put(Headers.CONTENT_TYPE, "text/plain");                    exchange.getResponseSender().send("foo" + exchange.getQueryParameters().get("id") + "\n");                });        server = Undertow.builder()                .addHttpListener(8080, "0.0.0.0", rootHandler)                .build();        server.start();        log.info("HttpActor Started");    }    private void stop() {        server.stop();        log.info("HttpActor Stopped");    }    public void onReceive(Object message) throws Exception {        if (message instanceof Start) {            this.start();        } else if (message instanceof Stop) {            this.stop();        } else {            unhandled(message);        }    }    public static class Start implements Serializable {    }    public static class Stop implements Serializable {    }}
--- src/main/java/org/bitvector/microservice2/Main.java
+++ src/main/java/org/bitvector/microservice2/Main.java
@@ -1,38 +1,1 @@
-package org.bitvector.microservice2;
-
-import akka.actor.ActorRef;
-import akka.actor.ActorSystem;
-import akka.actor.Props;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.Properties;
-
-public class Main {
-    public static void main(String[] args) {
-        Logger logger = LoggerFactory.getLogger(Main.class.getName());
-        logger.info("Starting Initialization");
-
-        String propertiesFile = "microservice2.properties";
-        ClassLoader loader = Thread.currentThread().getContextClassLoader();
-        try (InputStream resourceStream = loader.getResourceAsStream(propertiesFile)) {
-            if (resourceStream != null) {
-                Properties props = new Properties(System.getProperties());
-                props.load(resourceStream);
-                System.setProperties(props);
-            } else {
-                logger.error("Could not load properties file: " + propertiesFile);
-            }
-        } catch (IOException e) {
-            logger.error("Could not load properties file: " + propertiesFile, e);
-        }
-
-        ActorSystem system = ActorSystem.create("TheTheatre");
-        ActorRef httpActor = system.actorOf(Props.create(HttpActor.class), "HttpActor");
-        httpActor.tell(new HttpActor.Start(), ActorRef.noSender());
-
-        logger.info("Finished Initialization");
-    }
-}
+package org.bitvector.microservice2;import akka.actor.ActorRef;import akka.actor.ActorSystem;import akka.actor.Props;import org.slf4j.Logger;import org.slf4j.LoggerFactory;import java.io.IOException;import java.io.InputStream;import java.util.Properties;public class Main {    public static void main(String[] args) {        Logger logger = LoggerFactory.getLogger(Main.class.getName());        logger.info("Starting Initialization");        String propertiesFile = "microservice2.properties";        ClassLoader loader = Thread.currentThread().getContextClassLoader();        try (InputStream resourceStream = loader.getResourceAsStream(propertiesFile)) {            if (resourceStream != null) {                Properties props = new Properties(System.getProperties());                props.load(resourceStream);                System.setProperties(props);            } else {                logger.error("Could not load properties file: " + propertiesFile);            }        } catch (IOException e) {            logger.error("Could not load properties file: " + propertiesFile, e);        }        ActorSystem system = ActorSystem.create("TheTheatre");        ActorRef httpActor = system.actorOf(Props.create(HttpActor.class), "HttpActor");        httpActor.tell(new HttpActor.Start(), ActorRef.noSender());        logger.info("Finished Initialization");    }}
