e65f83e12a5cc454e10ae83474931f2973ff0631
Steven Logue <bitvector2@gmail.com>
2015-10-20 15:56:38 -0700

    moved watcher into init()

--- www/js/test.js
+++ www/js/test.js
@@ -1,4 +1,4 @@
-var app = angular.module('myApp', []);
+var app = angular.module('myApp', ['ngTouch']);
 
 app.controller('myCtrl', ['$scope', '$rootScope', '$http', '$window', function ($scope, $rootScope, $http, $window) {
 
